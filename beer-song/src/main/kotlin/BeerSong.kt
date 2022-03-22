d1b152abe05a6a1c2ae8958cbd7ace8f01731eb6
Jean Tadebois <jpm.tadebois@gmail.com>
2022-03-22 13:30:02 +1100

    Refactor Beer Song to replace if statement with when

--- beer-song/src/main/kotlin/BeerSong.kt
+++ beer-song/src/main/kotlin/BeerSong.kt
@@ -4,38 +4,43 @@ object BeerSong {
         var currentBottlesCount = startBottles
 
         for (i in startBottles downTo takeDown) {
-            if (currentBottlesCount == 0) {
-                strBuilder.append(
-                    """
-                    No more bottles of beer on the wall, no more bottles of beer.
-                    Go to the store and buy some more, 99 bottles of beer on the wall.
-                    
-                """.trimIndent()
-                )
-            } else if (currentBottlesCount == 1) {
-                strBuilder.append(
-                    """
-                    $currentBottlesCount bottle of beer on the wall, $currentBottlesCount bottle of beer.
-                    Take it down and pass it around, no more bottles of beer on the wall.
-                    
-                """.trimIndent()
-                )
-            } else if (currentBottlesCount == 2) {
-                strBuilder.append(
-                    """
-                    $currentBottlesCount bottles of beer on the wall, $currentBottlesCount bottles of beer.
-                    Take one down and pass it around, ${currentBottlesCount - 1} bottle of beer on the wall.
-                    
-                """.trimIndent()
-                )
-            } else {
-                strBuilder.append(
-                    """
-                    $currentBottlesCount bottles of beer on the wall, $currentBottlesCount bottles of beer.
-                    Take one down and pass it around, ${currentBottlesCount - 1} bottles of beer on the wall.
-                    
-                """.trimIndent()
-                )
+            when (currentBottlesCount) {
+                0 -> {
+                    strBuilder.append(
+                        """
+                            No more bottles of beer on the wall, no more bottles of beer.
+                            Go to the store and buy some more, 99 bottles of beer on the wall.
+                            
+                        """.trimIndent()
+                    )
+                }
+                1 -> {
+                    strBuilder.append(
+                        """
+                            $currentBottlesCount bottle of beer on the wall, $currentBottlesCount bottle of beer.
+                            Take it down and pass it around, no more bottles of beer on the wall.
+                            
+                        """.trimIndent()
+                    )
+                }
+                2 -> {
+                    strBuilder.append(
+                        """
+                            $currentBottlesCount bottles of beer on the wall, $currentBottlesCount bottles of beer.
+                            Take one down and pass it around, ${currentBottlesCount - 1} bottle of beer on the wall.
+                            
+                        """.trimIndent()
+                    )
+                }
+                else -> {
+                    strBuilder.append(
+                        """
+                            $currentBottlesCount bottles of beer on the wall, $currentBottlesCount bottles of beer.
+                            Take one down and pass it around, ${currentBottlesCount - 1} bottles of beer on the wall.
+                            
+                        """.trimIndent()
+                    )
+                }
             }
 
             if (currentBottlesCount != takeDown) {
